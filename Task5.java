26f902a87ab275afdcc512df6ef285462e2aeaf8
Al-DozoR <37379141+Al-DozoR@users.noreply.github.com>
2019-11-05 22:45:39 +0500

    Add files via upload

--- Task5.java
+++ Task5.java
@@ -1,11 +1,12 @@
-public class Task5 {
+public class Task5{
     public static void main(String[] args) {
-        String text="D18";
-        conversion(text);
-    }
-    static void conversion(String text){
-        String[] numColumn=text.split("[0-9]");
+        String text="ABC110";
+        short NumColumn = 0;
+        for(char ch : text.replaceAll("[0-9]","").toCharArray()){
+            NumColumn*=26;
+            NumColumn+=ch-('A'-1);
+        }
         System.out.println("Номер строки: "+text.replaceAll("\\D","") +
-                           " Номер столбца: "+(Integer.valueOf("ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(text.replaceAll("\\d","").toUpperCase()))+1));
+                " Номер столбца: "+NumColumn);
     }
 }
